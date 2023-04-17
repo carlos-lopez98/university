b8b16833e01ec8e1c575db3ea300466859b8d295
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-17 08:05:18 -0700

    Admission Service Implemented

--- src/main/java/com/solvd/university/service/AdmissionService.java
+++ src/main/java/com/solvd/university/service/AdmissionService.java
@@ -1,4 +1,20 @@
 package main.java.com.solvd.university.service;
 
+import main.java.com.solvd.university.repository.StudentRepo;
+
 public class AdmissionService {
+
+    private StudentRepo studentRepo;
+
+    public AdmissionService(StudentRepo studentRepo){
+        this.studentRepo = studentRepo;
+    }
+
+    public StudentRepo getStudents() {
+        return students;
+    }
+
+    public void setStudents(StudentRepo students) {
+        this.students = students;
+    }
 }
