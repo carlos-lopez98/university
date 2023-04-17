b9279ddd5ef792673f36c9b0a59d3284dd0357f4
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-17 07:55:05 -0700

    Added University Class

---
+++ src/main/java/com/solvd/university/models/University.java
@@ -0,0 +1,29 @@
+package main.java.com.solvd.university.models;
+
+public class University {
+
+    private int universityId;
+    private String universityName;
+
+
+    public University(String universityName){
+        this.universityName = universityName;
+    }
+
+    
+    public int getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(int universityId) {
+        this.universityId = universityId;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+}
