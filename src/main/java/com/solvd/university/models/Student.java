575c08aab07fe8a582569071750b2c88d97b41cf
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-17 07:40:02 -0700

    Adding Student Class

---
+++ src/main/java/com/solvd/university/models/Student.java
@@ -0,0 +1,36 @@
+package main.java.com.solvd.university.models;
+
+import java.time.ZonedDateTime;
+
+public class Student {
+
+    private String name;
+    private ZonedDateTime dateOfBirth;
+    private String address;
+    private int entryExamScore;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public ZonedDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(ZonedDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getEntryExamScore() {
+        return entryExamScore;
+    }
+    public void setEntryExamScore(int entryExamScore) {
+        this.entryExamScore = entryExamScore;
+    }
+}
