8787ac90164300b91d907a727660ef71e4bb095b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-19 15:28:59 -0700

    Adding SoccerClub Properties

--- src/main/java/com/solvd/university/models/clubs/SoccerClub.java
+++ src/main/java/com/solvd/university/models/clubs/SoccerClub.java
@@ -1,4 +1,32 @@
 package com.solvd.university.models.clubs;
 
+import com.solvd.university.models.persons.Student;
+
+import java.util.List;
+
 public class SoccerClub extends Club{
+
+    private List<Student> members;
+    private String meetingTimes;
+
+    public SoccerClub(String clubName) {
+        super(clubName);
+        this.meetingTimes = "Tues @ 1600 Hours, Sat @ 1600 Hours, Sun @ 1200 Hours";
+    }
+
+    public List<Student> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Student> members) {
+        this.members = members;
+    }
+
+    public String getMeetingTimes() {
+        return meetingTimes;
+    }
+
+    public void setMeetingTimes(String meetingTimes) {
+        this.meetingTimes = meetingTimes;
+    }
 }
