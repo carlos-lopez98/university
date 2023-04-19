0e86eae9c2fb259137560c19a254c2c5a9d73037
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-19 15:27:48 -0700

    Adding Club Properties

--- src/main/java/com/solvd/university/models/clubs/Club.java
+++ src/main/java/com/solvd/university/models/clubs/Club.java
@@ -1,4 +1,17 @@
 package com.solvd.university.models.clubs;
 
 public abstract class Club {
+    private String clubName;
+
+    public Club(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
 }
--- src/main/java/com/solvd/university/models/clubs/RoboticsClub.java
+++ src/main/java/com/solvd/university/models/clubs/RoboticsClub.java
@@ -1,4 +1,33 @@
 package com.solvd.university.models.clubs;
 
+import com.solvd.university.models.persons.Student;
+
+import java.util.List;
+
 public class RoboticsClub extends Club{
+
+    private List<Student> members;
+    private String meetingTimes;
+
+    public RoboticsClub(String clubName) {
+        super(clubName);
+
+        this.meetingTimes = "M @ 1200 Hours, Tues @ 1200 Hours, Fri @ 1900 Hours";
+    }
+
+    public List<Student> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Student> members) {
+        this.members = members;
+    }
+
+    public String getMeetingTimes() {
+        return meetingTimes;
+    }
+
+    public void setMeetingTimes(String meetingTimes) {
+        this.meetingTimes = meetingTimes;
+    }
 }
