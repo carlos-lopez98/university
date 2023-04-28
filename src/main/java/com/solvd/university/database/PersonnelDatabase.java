9ae5daec5e598d9e3466221948d516b4a0fc5b7c
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-28 06:49:26 -0700

    Adding StudentDatabase system methods

---
+++ src/main/java/com/solvd/university/database/PersonnelDatabase.java
@@ -0,0 +1,8 @@
+package com.solvd.university.database;
+
+public class PersonnelDatabase {
+
+
+
+
+}
--- src/main/java/com/solvd/university/database/StudentDatabase.java
+++ src/main/java/com/solvd/university/database/StudentDatabase.java
@@ -1,20 +1,40 @@
 package com.solvd.university.database;
 
+import com.solvd.university.Main;
+import com.solvd.university.exceptions.StudentNotFoundException;
 import com.solvd.university.models.persons.Student;
 import com.solvd.university.models.universities.University;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDatabase {
 
+    private Logger logger = LogManager.getLogger(Main.class);
     private List<Student> students;
 
     public StudentDatabase(UniversityDatabase database){
         int counter = 0;
         for(University university: database.getUniversityList()){
             students.addAll(database.getUniversityList().get(counter).getStudents());
+        }
+    }
+
+
+    private void addStudent(Student student){
+        students.add(student);
+        logger.info("Student was successfully added");
+    }
+
+    //TODO Remove student from UniversityDatabase
+    private void deleteByID(int studentId){
+        for(Student student: students){
+            if(student.getStudentId() == studentId){
+                students.remove(student);
+            }else {
+                throw new StudentNotFoundException("Student Id was not found in Student Database");
+            }
         }
     }
 }
